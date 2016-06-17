6a69d67a171844ba26deb219c0c763f1b2445add
kakas <shrimptrain@gmail.com>
2016-06-17 22:35:13 +0800

    增加產品 seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create!(title: "大麥客", price: 55)
+Product.create!(title: "可樂", price: 30)
+Product.create!(title: "中薯", price: 40)
+Product.create!(title: "大薯", price: 55)
+Product.create!(title: "雞塊", price: 45)
