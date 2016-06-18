76fc8aa85cf434057204db6ae21acc2ab9dc1a92
kakas <shrimptrain@gmail.com>
2016-06-18 14:13:10 +0800

    完成新增產品功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,25 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @products = Product.all # 給 _index.html.erb 用
+    @product = Product.new(product_params)
+
+    if @product.save
+      render "success"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :price)
+  end
+
 end
