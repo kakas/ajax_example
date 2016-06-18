d607b2b0d80a322bab334a3891d7b17bfd0a169b
kakas <shrimptrain@gmail.com>
2016-06-18 14:51:21 +0800

    完成編輯產品功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,21 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @products = Product.all
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      render "success"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
